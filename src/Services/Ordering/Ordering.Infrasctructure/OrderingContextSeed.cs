1a79dc10b98d64abdae156f821a470622036c3ad
Alex Freitas <alex.abreu.freitas@gmail.com>
2019-11-30 21:50:05 -0300

    OrderingContextSeed
    Added OrderingContextSeed

---
+++ src/Services/Ordering/Ordering.Infrasctructure/OrderingContextSeed.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Ordering.Domain.AggregatesModel.BuyerAggregate;
+using Ordering.Domain.AggregatesModel.OrderAggregate;
+using Ordering.Domain.SharedKernel;
+using Polly;
+using Polly.Retry;
+
+namespace Ordering.Infrastructure
+{
+    public class OrderingContextSeed
+    {
+        public async Task SeedAsync(OrderingSqliteDbContext context, ILogger<OrderingContextSeed> logger)
+        {
+            var policy = CreatePolicy(logger, nameof(OrderingContextSeed));
+
+            await policy.ExecuteAsync(async () =>
+            {
+                using (context)
+                {
+                    context.Database.Migrate();
+
+                    if (!context.CardTypes.Any())
+                    {
+                        context.CardTypes.AddRange(GetPredefinedCardTypes());
+                        await context.SaveChangesAsync();
+                    }
+
+                    if (!context.OrderStatus.Any())
+                    {
+                        context.OrderStatus.AddRange(GetPredefinedOrderStatus());
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+            });
+        }
+
+        private AsyncRetryPolicy CreatePolicy(ILogger<OrderingContextSeed> logger, string prefix, int retries = 3)
+        {
+            var policy = Policy.Handle<SqlException>()
+                .WaitAndRetryAsync(
+                retryCount: retries,
+                sleepDurationProvider: x => TimeSpan.FromSeconds(5),
+                onRetry: (exception, timeSpan, retry, context) =>
+                {
+                    logger.LogWarning(
+                        exception,
+                        "[{prefix}] Exception {ExceptionType} with message {Message} detected on attempt {retry} of {retries}",
+                        prefix,
+                        exception.GetType().Name,
+                        exception.Message,
+                        retry,
+                        retries);
+                });
+
+            return policy;
+        }
+
+        private IEnumerable<CardType> GetPredefinedCardTypes()
+        {
+            return Enumeration.GetAll<CardType>();
+        }
+
+        private IEnumerable<OrderStatus> GetPredefinedOrderStatus()
+        {
+            return new List<OrderStatus>()
+            {
+                OrderStatus.Submitted,
+                OrderStatus.AwaitingValidation,
+                OrderStatus.StockConfirmed,
+                OrderStatus.Paid,
+                OrderStatus.Shipped,
+                OrderStatus.Cancelled
+            };
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrasctructure/OrderingSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace Ordering.Infrastructure
+{
+    public class OrderingSettings
+    {
+        public bool UseCustomizationData { get; set; }
+     
+        public string ConnectionString { get; set; }
+
+        public string EventBusConnection { get; set; }
+
+        public int CheckUpdateTime { get; set; }
+    }
+}
